b78f2d7bd80677011678dad8f80be0db76a261d0
barlotem <barlotem@gmail.com>
2021-09-22 20:48:35 +0300

    Update: travel search pipe

--- src/app/pages/travel-collector/search.pipe.ts
+++ src/app/pages/travel-collector/search.pipe.ts
@@ -1,7 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Travel } from '../../@core/models/Travel';
 @Pipe({
-  name: 'travelFilter'
+  name: 'travelFilter',
+  pure: false
 })
 export class SearchPipe implements PipeTransform {
 
