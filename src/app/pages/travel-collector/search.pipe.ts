4afb6a7c3a8a4ef3bcfb101bfd0df5d25d4d8921
barlotem <barlotem@gmail.com>
2022-05-03 12:25:39 +0300

    Update: search pipline and result count

--- src/app/pages/travel-collector/search.pipe.ts
+++ src/app/pages/travel-collector/search.pipe.ts
@@ -6,21 +6,30 @@ import { Travel } from '../../@core/models/Travel';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
+  transform(value: any, args: string, filteredCount: any): any {
+    if (!value) {
+      return null;
+    }
     if (!args) {
+      filteredCount.count = value.length;
       return value;
     }
-    return value.filter((val: Travel) => {
-      let rVal = (val.country.toLocaleLowerCase().includes(args)) || 
-      (val.group.toLocaleLowerCase().includes(args)) || 
-      (val.track.toLocaleLowerCase().includes(args)) || 
-      (val.keyWords.toLocaleLowerCase().includes(args)) || 
-      (val.guide.toLocaleLowerCase().includes(args)) || 
-      (val.date.toLocaleString('he-IL').toLocaleLowerCase().includes(args)) || 
-      (val.albumId.toString().includes(args));
-      return rVal;
-    })
+    args = args.toLocaleLowerCase();
 
+    let filteredItems = value.filter(function(item) {
+        return JSON.stringify(item).toLocaleLowerCase().includes(args);
+    });
+    filteredCount.count = filteredItems.length;
+    return filteredItems;
   }
-
+    // return value.filter((val: Travel) => {
+    //   let rVal = (val.country.toLocaleLowerCase().includes(args)) || 
+    //   (val.group.toLocaleLowerCase().includes(args)) || 
+    //   (val.track.toLocaleLowerCase().includes(args)) || 
+    //   (val.keyWords.toLocaleLowerCase().includes(args)) || 
+    //   (val.guide.toLocaleLowerCase().includes(args)) || 
+    //   (val.date.toLocaleString('he-IL').toLocaleLowerCase().includes(args)) || 
+    //   (val.albumId.toString().includes(args));
+    //   return rVal;
+    // })
 }
--- src/app/pages/travel-collector/table/table.component.ts
+++ src/app/pages/travel-collector/table/table.component.ts
@@ -3,6 +3,7 @@ import { NbDialogService } from '@nebular/theme';
 import { Travel } from '../../../@core/models/Travel';
 import { TravelService } from '../../../@core/utils/Travel.service';
 import { TravelDialogComponent } from '../travel-dialog/travel-dialog.component';
+import { ChangeDetectorRef, AfterContentChecked} from '@angular/core';
 
 @Component({
   selector: 'ngx-table',
@@ -13,21 +14,26 @@ export class TableComponent implements OnInit {
 
   travelList: Travel[];
   searchTerm: string;
+  filteredCount: any;
 
-  constructor(private travelService: TravelService, private dialogService: NbDialogService) {
+  constructor(private travelService: TravelService, private dialogService: NbDialogService, private cdref: ChangeDetectorRef) {
     this.travelList = new Array<Travel>();
     this.searchTerm = "";
+    this.filteredCount = { count: 0 };
    }
 
   ngOnInit() : void{
     this.travelService.getTravelList().subscribe((result: Travel[])=> {
       this.travelList = result; 
-
       if (this.travelList == null ) {
         // Get from local storage
         this.travelList = JSON.parse(localStorage.getItem("travelList") || "[]") as Travel[];
       }
-    }); 
+    });
+  }
+
+  ngAfterContentChecked() {
+    this.cdref.detectChanges();
   }
 
   OpenDialog(travel: Travel) {
